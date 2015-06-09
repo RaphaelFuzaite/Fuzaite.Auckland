82be9453113cb6f26b5b20ff67b673e6807e3b59
medcbbti <ti@cbbw.com.br>
2015-06-09 12:04:25 -0300

    Arquivo de configuração do travis

--- Public/App.js
+++ Public/App.js
@@ -1,3 +1,7 @@
 'use strict';
 
 angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleNameDependencies);
+
+angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider', function($locationProvider){
+	$locationProvider.hasPrefix('!');
+}]);
